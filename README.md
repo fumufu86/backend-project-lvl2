e01c84d9965e26550bf1e39ee801070a4d4464e5
nimda <fumufu86@gmail.com>
2021-12-08 14:27:25 +0000

    step3

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/fumufu86/backend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/fumufu86/backend-project-lvl2/actions)
+
+https://asciinema.org/a/LqSEcSlI5bUF9mR0IzrOHXyul
--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,31 @@
 import _ from 'lodash';
+import fs from 'fs';
 
-
-const parser = () => console.log('test');
-
-
-
+const readFile = (filepath) => fs.readFileSync(filepath, 'utf8');
+const parser = (filepath1, filepath2) => {
+ const obj1 = JSON.parse(readFile(filepath1));
+ const obj2 = JSON.parse(readFile(filepath2));
+ //console.log(filepath1);
+ //console.log(obj1);
+ //const keys = _.sortedUniq(_.union(Object.keys(obj1), Object.keys(obj2)));
+ const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
+ let result = `{\n`;
+ for (const key of keys) {
+   if (obj1[key] !== obj2[key] && _.has(obj1, key) && _.has(obj2, key)) {
+     result = `${result}  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}\n`
+   } else if (obj1[key] === obj2[key] && _.has(obj1, key) && _.has(obj2, key)) {
+     result = `${result}    ${key}: ${obj1[key]}\n`
+   } else if (_.has(obj1, key) && !_.has(obj2, key)) {
+     result = `${result}  - ${key}: ${obj1[key]}\n`
+   } else if (!_.has(obj1, key) && _.has(obj2, key)) {
+     result = `${result}  + ${key}: ${obj2[key]}\n`
+   }
+ }
+ result = `${result}}`;
+ //console.log(keys)
+ //console.log(_.sortedUniq(_.concat(Object.keys(obj1), Object.keys(obj2))));
+ //console.log(_.sortBy(_.union(Object.keys(obj1), Object.keys(obj2))));
+ console.log(result);
+};
 
 export default parser;
