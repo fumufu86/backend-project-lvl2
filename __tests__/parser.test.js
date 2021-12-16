202ba6929e64cec4e339e3f673aee831256fa16f
nimda <fumufu86@gmail.com>
2021-12-16 19:02:30 +0000

    add test

---
+++ __tests__/parser.test.js
@@ -0,0 +1,19 @@
+import gendiff from '../src/parser.js';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { expect, test } from '@jest/globals';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('test parser.js', () => {
+    const firstFile = getFixturePath('file1.json');
+    const secondFile = getFixturePath('file2.json');
+    const expectedResult = readFile('result.json');
+    const result = gendiff(firstFile, secondFile);
+    expect(result).toEqual(expectedResult);
+});
