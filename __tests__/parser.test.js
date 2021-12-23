a1b7e0f860ed53e68a4003cb03c062e7e959c19c
nimda <fumufu86@gmail.com>
2021-12-23 06:31:35 +0000

    step 5

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -10,10 +10,18 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('test parser.js', () => {
+test('test parser.js for JSON', () => {
   const firstFile = getFixturePath('file1.json');
   const secondFile = getFixturePath('file2.json');
-  const expectedResult = readFile('result.json');
+  const expectedResult = readFile('result.txt');
+  const result = gendiff(firstFile, secondFile);
+  expect(result).toEqual(expectedResult);
+});
+
+test('test parser.js for YAML', () => {
+  const firstFile = getFixturePath('file1.yml');
+  const secondFile = getFixturePath('file2.yml');
+  const expectedResult = readFile('result.txt');
   const result = gendiff(firstFile, secondFile);
   expect(result).toEqual(expectedResult);
 });
--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,23 @@
 import _ from 'lodash';
+import yaml from 'js-yaml';
 import fs from 'fs';
 
 const readFile = (filepath) => fs.readFileSync(filepath, 'utf8');
+const parseFile = (filepath) => {
+  const lastDot = filepath.lastIndexOf('.');
+  const format = filepath.slice(lastDot);
+  switch (format) {
+    case '.json':
+      return JSON.parse(readFile(filepath));
+    case ('.yml' ||'.yaml'):
+      return yaml.load(readFile(filepath));
+    default:
+      throw new Error(`Формат не поддерживается: ${format}`);
+  }
+}
 const parser = (filepath1, filepath2) => {
-  const obj1 = JSON.parse(readFile(filepath1));
-  const obj2 = JSON.parse(readFile(filepath2));
+  const obj1 = parseFile(filepath1);
+  const obj2 = parseFile(filepath2);
   const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
   const func = (acc, key) => {
     if (obj1[key] !== obj2[key] && _.has(obj1, key) && _.has(obj2, key)) {
